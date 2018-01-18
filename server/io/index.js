a4140c8e2c6f19e0d0455cb210cba5d38326874b
webdevcrazier <devcrazier@gmail.com>
2018-01-19 02:47:29 +0800

    remove io

--- server/io/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = function(io) {
-
-  /*Include trade*/
-  require('./trade/trade.js')(io);
-
-};
--- server/io/notifications.js
+++
@@ -1,6 +0,0 @@
-var emitter = require('./emitter');
-module.exports = function (io) {
-  emitter.on('notification', function (data) {
-    io.emit('notification', data);
-  });
-};
